35debc9e606f9e53b9ff7ccb52ec5c42ca83a0ab
DmitriyGrigorash <dmitriygrigorash90@gmail.com>
2019-06-28 21:12:40 +0300

    Add check NODE_ENV variable

--- index.js
+++ index.js
@@ -40,6 +40,16 @@ if (port == null || port == "") {
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 
 // *** Express listen
 app.listen(port, () => {
