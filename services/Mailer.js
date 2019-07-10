1100215bc935b862c6aefb1dc3b804ab60d0c894
DmitriyGrigorash <dmitriygrigorash90@gmail.com>
2019-07-10 20:12:45 +0300

    Mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,8 +1,27 @@
 const sgMail = require('@sendgrid/mail');
+
 const keys = require('../config/keys');
 
+sgMail.setApiKey(keys.sendGridKey);
 class Mailer extends sgMail {
+    constructor({subject, recipients}, content) {
+        super();
 
+        this.recipients = this.formatAddresses(recipients);
+        this.msg = {
+            to: this.recipients,
+            from: 'no-reply@emaily.com',
+            subject: subject,
+            text: 'and easy to do anywhere, even with Node.js',
+            html: content
+        };
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return email;
+        })
+    }
 }
 
 module.exports = Mailer;
