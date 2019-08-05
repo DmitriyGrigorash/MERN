4c4d0df68ed9875807d9a2619a72343514ddf91c
Dmitriy <dmitriygrigorash90@gmail.com>
2019-08-05 15:48:54 +0300

    create survey field

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import Button from "@material-ui/core/Button";
+import SurveyField from './SurveyField';
 
 export class SurveyForm extends React.Component {
+    renderFields() {
+        return (
+            <Field name="surveyTitle" component={SurveyField} type="text" />
+        )
+    }
     render() {
         return (
             <article>
                 <h3>Form Survey</h3>
                 <form onSubmit={this.props.handleSubmit(values => console.log('### values', values))}>
-                    <Field name="surveyTitle" component="input" type="text" />
+                    
                     <Button color="default" size="medium" type="submit">Submit</Button>
                 </form>
             </article>
