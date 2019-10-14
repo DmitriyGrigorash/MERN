3deec062cd15b7c2060585511b95c56d02491aed
Dmitriy <dmitriygrigorash90@gmail.com>
2019-10-14 21:14:16 +0300

    find events

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const Path = require('path-parser').default;
 
 const requireLogin = require("../middleware/requireLogin");
 const requireCredits = require("../middleware/requireCredits");
@@ -40,8 +42,21 @@ module.exports = app => {
     }]);
 
     app.post("/api/surveys/webhooks", (req, res) => {
-        const events = req.body.map(({email, url}) => {
-            const pathname = new URL(url).pathname;
-        });
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
+        const events = _.chain(req.body)
+            .map(( {email, url} ) => {
+                const match = p.test(new URL(url).pathname);
+                if( match ) {
+                    return { email, surveyId: match.surveyId, choice: match.choice }
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .value();
+
+        console.log('### events', events);
+
+        res.send({})
     });
 };
