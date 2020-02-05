bca2b1aa1ac4da1a37fd8f6b2713418dc14590c4
Dmitriy <dmitriygrigorash90@gmail.com>
2020-02-05 12:04:33 +0200

    update store

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {createStore, applyMiddleware, compose } from 'redux';
 import {BrowserRouter} from 'react-router-dom';
-import reduxThunk from 'redux-thunk';
 import {StripeProvider} from 'react-stripe-elements';
 
 import App from "./components/App";
-import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(
-    applyMiddleware(reduxThunk)
-));
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
